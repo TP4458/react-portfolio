13d0e4733343844d22aa2c665737f284139eef08
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-23 14:53:16 +0000

    nav links configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx';
+import './index.css';
+import Main from './pages/Home.jsx';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Projects',
+        element: <Projects />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
